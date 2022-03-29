1f33b7e42f38ef32d53fe8d6c51ec9db49b69e45
LounesBrahimi <lounesnewgs5@gmail.com>
2022-03-29 13:46:20 +0200

    Correcting some bugs

--- the_social_media/src/app/posting-service.service.ts
+++ the_social_media/src/app/posting-service.service.ts
@@ -10,6 +10,6 @@ export class PostingServiceService {
   constructor(private http:HttpClient) { }
 
   public doPosting(post: Post){
-    return this.http.post("http://localhost:9010/post",post,{responseType:'text' as 'json'});
+    return this.http.post<Post>("http://localhost:9010/post",post,{responseType:'text' as 'json'});
   }
 }
